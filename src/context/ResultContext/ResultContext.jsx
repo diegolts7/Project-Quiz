efe19959e6af9b6b1ab4a19f45945d595a159833
Diego Sousa <diego7lts7@gmail.com>
2024-07-27 18:36:16 -0300

    implementando a funcionalidade e salvar os acertos na localStorage.

--- src/context/ResultContext/ResultContext.jsx
+++ src/context/ResultContext/ResultContext.jsx
@@ -1,9 +1,13 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 const ContextResult = createContext();
 
 const ResultContext = ({ children }) => {
-  const [acertosTotais, setAcertosTotais] = useState(0);
+  const [acertosTotais, setAcertosTotais] = useState(
+    localStorage.getItem("questoesCertas") !== null
+      ? Number(localStorage.getItem("questoesCertas"))
+      : 0
+  );
   const [questoesRespondidas, setQuestoesRespondidas] = useState(0);
 
   function toggleAcertosTotais() {
@@ -14,6 +18,26 @@ const ResultContext = ({ children }) => {
       (questoesRespondidasBefore) => questoesRespondidasBefore + 1
     );
   }
+
+  useEffect(() => {
+    if (localStorage.getItem("questoesCertas") !== null) {
+      let nQuestoesCertas = Number(localStorage.getItem("questoesCertas"));
+      localStorage.setItem("questoesCertas", `${nQuestoesCertas + 1}`);
+      console.log(nQuestoesCertas);
+    } else if (acertosTotais > 0) {
+      localStorage.setItem("questoesCertas", `${acertosTotais}`);
+    }
+  }, [acertosTotais]);
+
+  /*useEffect(() => {
+    if (localStorage.getItem("questoesRespondidas") !== null) {
+      let nQuestoesCertas = JSON.parse(localStorage.getItem("questoesRespondidas"));
+      localStorage.setItem("questoesCertas", `${nQuestoesCertas + 1}`);
+      console.log(nQuestoesCertas);
+    } else if (acertosTotais > 0) {
+      localStorage.setItem("questoesCertas", `${acertosTotais}`);
+    }
+  }, [questoesRespondidas]);*/
 
   return (
     <ContextResult.Provider
